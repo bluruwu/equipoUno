a6067cc455b266a83a9d39cadaebe451a026f378
HectorDiiaz <hectorluis_d@hotmail.es>
2023-12-06 17:44:13 -0500

    Navegacion hacia inventory al guardar

--- app/src/main/java/com/univalle/widgetinventory/view/fragment/AddFragment.kt
+++ app/src/main/java/com/univalle/widgetinventory/view/fragment/AddFragment.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.firestore.FirebaseFirestore
 import com.univalle.widgetinventory.R
 import com.univalle.widgetinventory.databinding.FragmentAddBinding
@@ -72,7 +73,7 @@ class AddFragment : Fragment() {
 
             )
 
-            Toast.makeText(context, "Articulo guardado", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addFragment_to_inventoryFragment)
         } else {
             Toast.makeText(context, "Llene los campos", Toast.LENGTH_SHORT).show()
         }
