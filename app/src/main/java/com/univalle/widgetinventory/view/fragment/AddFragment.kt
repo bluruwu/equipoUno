4b1542a69487c7c0a85b4c2a07593b66fa4ca6a6
HectorDiiaz <hectorluis_d@hotmail.es>
2023-12-06 16:24:47 -0500

    Agregar producto funcionando

--- app/src/main/java/com/univalle/widgetinventory/view/fragment/AddFragment.kt
+++ app/src/main/java/com/univalle/widgetinventory/view/fragment/AddFragment.kt
@@ -1,60 +1,65 @@
 package com.univalle.widgetinventory.view.fragment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import com.univalle.widgetinventory.R
+import com.univalle.widgetinventory.databinding.FragmentAddBinding
+import com.univalle.widgetinventory.model.Producto
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentAddBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedPreferences = requireActivity().getSharedPreferences("shared", Context.MODE_PRIVATE)
+        setup()
     }
-}
+
+    private fun guardarProducto() {
+        val codigo = binding.etCodigoProducto.text.toString()
+        val nombre = binding.etNombreArticulo.text.toString()
+        val precio = binding.etPrecio.text.toString()
+        val cantidad = binding.etCantidad.text.toString()
+
+        if (codigo.isNotEmpty() && nombre.isNotEmpty() && precio.isNotEmpty()) {
+            val producto = Producto(codigo.toInt(), nombre, precio.toInt(), cantidad.toInt())
+
+            db.collection("producto").document(producto.codigo.toString()).set(
+                hashMapOf(
+                    "codigo" to producto.codigo,
+                    "nombre" to producto.nombre,
+                    "precio" to producto.precio,
+                    "cantidad" to  producto.cantidad
+                )
+
+            )
+
+            Toast.makeText(context, "Articulo guardado", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Llene los campos", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun setup() {
+        binding.btnAdd.setOnClickListener {
+            guardarProducto()
+        }
+    }
+    }
