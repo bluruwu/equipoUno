ea0978f2cd85ca7ce728317668197cd3b3faee9b
stiven-acua17 <brayan.acuna@correounivalle.edu.co>
2023-12-15 00:34:28 -0500

    Correciones arquitectura MVVM

--- app/src/main/java/com/univalle/widgetinventory/repository/InventoryRepository.kt
+++ app/src/main/java/com/univalle/widgetinventory/repository/InventoryRepository.kt
@@ -1,6 +1,7 @@
 package com.univalle.widgetinventory.repository
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.univalle.widgetinventory.model.Producto
 import kotlinx.coroutines.tasks.await
@@ -17,4 +18,23 @@ class InventoryRepository {
             )
         ).await()
     }
+
+    suspend fun editarProducto(codigo: String, producto: Producto) {
+        db.collection("producto").document(codigo).update(
+            hashMapOf<String, Any> (
+                "nombre" to producto.nombre,
+                "precio" to producto.precio,
+                "cantidad" to producto.cantidad
+            )
+        ).await()
+    }
+
+    suspend fun obtenerProducto(codigo: Int): DocumentSnapshot? {
+        return try {
+            db.collection("producto").document(codigo.toString()).get().await()
+        } catch (e: Exception) {
+            null
+        }
+    }
+
 }
