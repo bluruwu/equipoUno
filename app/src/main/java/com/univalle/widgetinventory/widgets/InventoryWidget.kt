8fc1626afa6e366fafce8c2fda202d4b8a5ae444
@Bluruwu <juandiegotres@hotmail.com>
2023-12-15 15:56:57 -0500

    .

--- app/src/main/java/com/univalle/widgetinventory/widgets/InventoryWidget.kt
+++ app/src/main/java/com/univalle/widgetinventory/widgets/InventoryWidget.kt
@@ -12,6 +12,7 @@ import android.widget.RemoteViews
 import com.univalle.widgetinventory.R
 import com.univalle.widgetinventory.repository.InventoryRepository
 import com.univalle.widgetinventory.view.LoginActivity
+import com.univalle.widgetinventory.view.fragment.InventoryHomeFragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -28,11 +29,18 @@ class InventoryWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray,
     ) {
+        val widget = context.getSharedPreferences("eyePreferences", Context.MODE_PRIVATE)
+        val eyeState = widget.getBoolean("eye_state", false)
+        val views = RemoteViews(context.packageName, R.layout.inventory_widget)
+        val changeImage = if (eyeState) R.drawable.eye else R.drawable.noeye
+        views.setImageViewResource(R.id.eyeToggle, changeImage)
+
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId, inventoryRepository)
         }
     }
+
     private fun isUserLogged(context: Context): Boolean {
         sharedPreferences = context.getSharedPreferences("shared", Context.MODE_PRIVATE)
         val email = sharedPreferences.getString("email", null)
@@ -50,29 +58,35 @@ class InventoryWidget : AppWidgetProvider() {
         editor?.putBoolean("eye_state", isOpen)
         editor?.apply()
     }
+
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
         Log.d("Alerta", "onrecieve")
-        Log.d("Alerta", "ESte es_ "+ intent?.action)
-        if(intent?.action == "TOGGLE_EYE"){
+        Log.d("Alerta", "ESte es_ " + intent?.action)
+        if (intent?.action == "TOGGLE_EYE") {
             val eyeState = context?.let { getEyeState(it) }
             Log.d("Alerta", "leo accion")
-            if(eyeState == true) {
+            if (eyeState == true) {
                 Log.d("Alerta", "Estaba encendido")
-                if(isUserLogged(context)){
+                if (isUserLogged(context)) {
                     Log.d("Alerta", "estoy logueado")
                     saveEyeState(context, false)
                     AppWidgetManager.getInstance(context).let { appWidgetManager ->
                         val thisWidget = ComponentName(context, InventoryWidget::class.java)
                         val appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget)
                         for (appWidgetId in appWidgetIds) {
-                            updateAppWidget(context, appWidgetManager, appWidgetId, inventoryRepository)
+                            updateAppWidget(
+                                context,
+                                appWidgetManager,
+                                appWidgetId,
+                                inventoryRepository
+                            )
                         }
                     }
-                } else{
-                    val login = Intent(context, LoginActivity::class.java).apply{
+                } else {
+                    val login = Intent(context, LoginActivity::class.java).apply {
                         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        putExtra("Redirect", true )
+                        putExtra("Redirect", true)
                     }
                     context.startActivity(login)
                     Log.d("Alerta", "no estoy logueado")
@@ -88,63 +102,79 @@ class InventoryWidget : AppWidgetProvider() {
                     }
                 }
             }
-        }else if (intent?.action == "OPEN_INVENTORY"){
+        } else if (intent?.action == "OPEN_INVENTORY") {
             Log.d("Alerta", "soy open")
+            if (isUserLogged(context!!)) {
+                val inventory = Intent(context, InventoryHomeFragment::class.java).apply {
+
+                }
+
+            } else {
+
+            }
         } else {
             Log.d("Alerta", "Ahh")
         }
     }
 }
+    internal fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        inventoryRepository: InventoryRepository
+    ) {
+        Log.d("Alerta", "HOLAAAAAAA")
+        val widget = context.getSharedPreferences("eyePreferences", Context.MODE_PRIVATE)
+        val eyeState = widget.getBoolean("eye_state", false)
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int,
-    inventoryRepository: InventoryRepository
-) {
-    Log.d("Alerta", "HOLAAAAAAA")
-    val widget = context.getSharedPreferences("eyePreferences", Context.MODE_PRIVATE)
-    val eyeState = widget.getBoolean("eye_state", false)
+        val changeImage = if (eyeState) R.drawable.eye else R.drawable.noeye
+        val views = RemoteViews(context.packageName, R.layout.inventory_widget)
+        views.setImageViewResource(R.id.eyeToggle, changeImage)
 
-    val changeImage = if (eyeState) R.drawable.eye else R.drawable.noeye
-    val views = RemoteViews(context.packageName, R.layout.inventory_widget)
-    views.setImageViewResource(R.id.eyeToggle, changeImage)
-
-    if(eyeState){
-        views.setTextViewText(R.id.value, "$ * * * *")
-        appWidgetManager.updateAppWidget(appWidgetId, views)
-    } else {
-        CoroutineScope(Dispatchers.IO).launch {
-            val total = inventoryRepository.getTotalArticles()
-            val format = NumberFormat.getNumberInstance(Locale("es", "ES"))
-            format.minimumFractionDigits=2
-            val formatTotal = format.format(total)
-            CoroutineScope(Dispatchers.Main).launch {
-                views.setTextViewText(R.id.value, "$ $formatTotal")
-                appWidgetManager.updateAppWidget(appWidgetId, views)
+        if(eyeState){
+            views.setTextViewText(R.id.value, "$ * * * *")
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        } else {
+            CoroutineScope(Dispatchers.IO).launch {
+                val total = inventoryRepository.getTotalArticles()
+                val format = NumberFormat.getNumberInstance(Locale("es", "ES"))
+                format.minimumFractionDigits=2
+                val formatTotal = format.format(total)
+                CoroutineScope(Dispatchers.Main).launch {
+                    views.setTextViewText(R.id.value, "$ $formatTotal")
+                    appWidgetManager.updateAppWidget(appWidgetId, views)
+                }
             }
         }
+
+        val intentEye = Intent(context, InventoryWidget::class.java)
+        intentEye.action = "TOGGLE_EYE"
+        intentEye.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+        val pendingIntentEye = PendingIntent.getBroadcast(
+            context,
+            0,
+            intentEye,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        views.setOnClickPendingIntent(R.id.eyeToggle, pendingIntentEye)
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+
+        val intentOpenInv = Intent(context, InventoryWidget::class.java)
+        intentOpenInv.action = "OPEN_INVENTORY"
+
+        val pendingIntentOpenInv = PendingIntent.getBroadcast(
+            context,
+            1,
+            intentOpenInv,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        views.setOnClickPendingIntent(R.id.configImage, pendingIntentOpenInv)
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+
     }
 
-    val intentEye = Intent(context, InventoryWidget::class.java)
-    intentEye.action = "TOGGLE_EYE"
-    val pendingIntentEye = PendingIntent.getBroadcast(
-        context,
-        0,
-        intentEye,
-        PendingIntent.FLAG_IMMUTABLE
-    )
-    views.setOnClickPendingIntent(R.id.eyeToggle, pendingIntentEye)
 
-    val intentOpenInv = Intent(context, InventoryWidget::class.java)
-    intentOpenInv.action = "OPEN_INVENTORY"
 
-    val pendingIntentOpenInv = PendingIntent.getBroadcast(
-        context,
-        1,
-        intentOpenInv,
-        PendingIntent.FLAG_IMMUTABLE
-    )
-    views.setOnClickPendingIntent(R.id.configImage, pendingIntentOpenInv)
-}
+
+
 
