5c922648be8c288850fff35793aa371cd36a032f
@Bluruwu <juandiegotres@hotmail.com>
2023-12-14 22:30:54 -0500

    ignorar

--- app/src/main/java/com/univalle/widgetinventory/view/fragment/InventoryHomeFragment.kt
+++ app/src/main/java/com/univalle/widgetinventory/view/fragment/InventoryHomeFragment.kt
@@ -1,60 +1,30 @@
 package com.univalle.widgetinventory.view.fragment
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.univalle.widgetinventory.R
+import com.univalle.widgetinventory.databinding.FragmentEditBinding
+import com.univalle.widgetinventory.databinding.FragmentInventoryBinding
+import com.univalle.widgetinventory.viewmodel.InventoryViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [InventoryHomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InventoryHomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentInventoryBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    private val inventoryViewModel : InventoryViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
+       override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inventory, container, false)
+           binding = FragmentInventoryBinding.inflate(inflater)
+           binding.lifecycleOwner = this
+           return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InventoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InventoryHomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
